01460f2255b67ec5335853743e8db6d55571d854
yaweiwang <xdjm562@qq.com>
2023-09-20 16:18:02 +0800

    support cache

--- huggingface/base/download.py
+++ huggingface/base/download.py
@@ -1,9 +1,15 @@
+import traceback
 from huggingface_hub import snapshot_download
 
-def wrapper(repo_id: str, ignore_regex: list = []):
+import os
+
+
+def wrapper(repo_id: str, ignore_patterns: list = None, allow_patterns: list = None, cache_dir: str = None):
     try:
-        snapshot_download(repo_id=repo_id, ignore_regex=ignore_regex)
+        snapshot_download(repo_id=repo_id, ignore_patterns=ignore_patterns, allow_patterns=allow_patterns, cache_dir=cache_dir)
     except:
-        pass
+        traceback.print_exc()
     
-# wrapper(repo_id="decapoda-research/llama-7b-hf", ignore_regex=["*.h5", "*.ot", "*.msgpack"])
+# wrapper(repo_id="decapoda-research/llama-7b-hf", ignore_patterns=["*.h5", "*.ot", "*.msgpack"])
+wrapper(repo_id="ShilongLiu/GroundingDINO", cache_dir=)
+# wrapper(repo_id="lkeab/hq-sam")
