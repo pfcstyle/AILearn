c69aa51c305db5ea8935fed544b3a01c718ff6cd
yaweiwang <xdjm562@qq.com>
2024-04-08 17:33:35 +0800

    learn diffusion learn clip learn transformer learn dalle

--- huggingface/base/download.py
+++ huggingface/base/download.py
@@ -3,13 +3,14 @@ from huggingface_hub import snapshot_download
 
 import os
 
+access_token = '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'
 
 def wrapper(repo_id: str, ignore_patterns: list = None, allow_patterns: list = None, cache_dir: str = None):
     try:
-        snapshot_download(repo_id=repo_id, ignore_patterns=ignore_patterns, allow_patterns=allow_patterns, cache_dir=cache_dir)
+        snapshot_download(repo_id=repo_id, ignore_patterns=ignore_patterns, allow_patterns=allow_patterns, cache_dir=cache_dir, resume_download=True, local_dir_use_symlinks=False, token=access_token)
     except:
         traceback.print_exc()
     
 # wrapper(repo_id="decapoda-research/llama-7b-hf", ignore_patterns=["*.h5", "*.ot", "*.msgpack"])
-wrapper(repo_id="ShilongLiu/GroundingDINO", cache_dir=)
+wrapper(repo_id="stabilityai/sv3d")
 # wrapper(repo_id="lkeab/hq-sam")
